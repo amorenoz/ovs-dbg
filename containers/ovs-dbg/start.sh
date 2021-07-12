397e19db2936968c3f9fbbebb0b2a80b9851d959
Adrian Moreno <amorenoz@redhat.com>
2021-07-12 16:36:34 +0200

    poc: add a tool to perform offline troubleshooting
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

---
+++ containers/ovs-dbg/start.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -mex
+
+vswitchd-dummy() {
+  echo "=============== vswitchd-dummy ==============="
+  echo "RESTORE_DIR=$RESTORE_DIR"
+
+  (
+  echo "Starting flow restoration. Waiting for ovs-vswitchd to start..."
+  sleep 5
+
+  if [ -d "$RESTORE_DIR" ]; then
+      echo "Restoring flows"
+      for file in $(ls -x $RESTORE_DIR/*flows.dump); do
+          # fix igmp flows
+          sed -i 's/igmp,/ip,nw_proto=2,/g' $file
+      done
+      # do not remove directory
+      sed -i '/rm.*/d' $RESTORE_DIR/restore.sh
+      sh -e $RESTORE_DIR/restore.sh
+  fi
+  ) &
+
+  ovs-vswitchd --enable-dummy=override -vvconn -vnetdev_dummy  --no-chdir --pidfile  -vsyslog:off
+
+}
+
+ovsdb() {
+  OVSDB_BACKUP=${OVSDB_BACKUP:-/usr/local/var/run/openvswitch/db.backup}
+  echo "=============== ovsdb-server ==============="
+  echo "OVSDB_BACKUP=$OVSDB_BACKUP"
+
+  if [ -f ${OVSDB_BACKUP} ]; then
+          echo "Using backup DB file at $OVSDB_BACKUP"
+          cp $OVSDB_BACKUP /usr/local/etc/openvswitch/conf.db
+  else
+          echo "Creating new DB"
+
+          #sleep infinity
+          ovsdb-tool create /usr/local/etc/openvswitch/conf.db /usr/local/share/openvswitch/vswitch.ovsschema
+  fi
+
+  ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock --remote=ptcp:6640 --pidfile=ovsdb-server.pid
+
+}
+
+cmd=${1:-""}
+case ${cmd} in
+"vswitchd-dummy")
+  vswitchd-dummy
+  ;;
+"ovsdb")
+  ovsdb
+  ;;
+"*")
+  echo "Command not supported: ${cmd}"
+  exit 1
+esac
---
+++ docs/source/ofofproto.rst
@@ -0,0 +1,12 @@
+(PoC) of-ofproto-trace
+======================
+
+
+Tool to run ofproto/trace commands ofline
+
+Requirements:
+
+- docker
+
+
+TBD
