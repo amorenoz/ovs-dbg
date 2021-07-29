6b64220281c2c9b0dfaf052737b511c001d1bc7c
Adrian Moreno <amorenoz@redhat.com>
2021-07-29 17:37:30 +0200

    fix still missing dependencies
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-requirements = ["click", "rich"]
+requirements = ["click", "rich", "pyparsing", "netaddr"]
 
 setup_requirements = [
     "pytest-runner",
